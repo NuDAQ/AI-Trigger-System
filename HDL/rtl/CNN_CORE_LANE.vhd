bfdbf06846228bf760c62b4cb386ad133580c97e
Albert <158596352+albertc9@users.noreply.github.com>
2026-07-06 19:15:57 -0700

    revert cnn lane fifo read alignment

--- HDL/rtl/CNN_CORE_LANE.vhd
+++ HDL/rtl/CNN_CORE_LANE.vhd
@@ -265,8 +265,6 @@ begin
     -- -------------------------------------------------------------------------
     -- CNN stream FSM + FIFO read logic (mirrors original CNN_FIFO_CONNECTOR)
     -- -------------------------------------------------------------------------
-    fifo_rd_en <= cnn_in_valid and cnn_in_ready;
-
     process(CLK_CNN)
     begin
         if rising_edge(CLK_CNN) then
@@ -274,6 +272,7 @@ begin
                 cnn_state    <= CC_IDLE;
                 cnn_start    <= '0';
                 cnn_in_valid <= '0';
+                fifo_rd_en   <= '0';
                 stream_cnt   <= 0;
                 started_count_cnn <= (others => '0');
                 stream_done_toggle_cnn <= '0';
@@ -291,6 +290,7 @@ begin
                 chunk_id_dest_seen <= '0';
             else
                 lane_valid_r <= '0';
+                fifo_rd_en   <= '0';   -- default: don't pop
                 chunk_id_dest_ack <= '0';
                 if chunk_id_dest_req = '0' then
                     chunk_id_dest_seen <= '0';
@@ -366,6 +366,7 @@ begin
 
                         if cnn_in_ready = '1' then
                             stream_cnt <= stream_cnt - 1;
+                            fifo_rd_en <= '1';
 
                             if stream_cnt = 1 then
                                 stream_done_toggle_cnn <= not stream_done_toggle_cnn;
